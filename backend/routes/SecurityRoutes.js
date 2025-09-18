402a2e771cc0c01b7cea4949aa61197be25be980
Ilyas <ilyasalabi05@gmail.com>
2025-09-18 23:31:27 +0100

    Remove Backend

--- backend/routes/SecurityRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getSecurityEvents } = require("../controllers/securityController");
-const {protect} = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.get("/events", protect, getSecurityEvents);
-
-module.exports = router;
--- backend/routes/crypto.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getOHLC,getMarkets } = require("../controllers/cryptoController");
-
-router.get("/:coin/ohlc", getOHLC);
-router.get("/markets", getMarkets); 
-module.exports = router;
